af8403a62e82c7b306aba9811b47d9615a07f8bc
sai wanza <q020385791@gmail.com>
2020-07-02 14:21:01 +0800

    加入專案檔案。

---
+++ GoogleEmail/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GoogleEmail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendMail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SendMail
+            // 
+            this.SendMail.Location = new System.Drawing.Point(24, 250);
+            this.SendMail.Name = "SendMail";
+            this.SendMail.Size = new System.Drawing.Size(75, 23);
+            this.SendMail.TabIndex = 0;
+            this.SendMail.Text = "SendMail";
+            this.SendMail.UseVisualStyleBackColor = true;
+            this.SendMail.Click += new System.EventHandler(this.SendMail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 299);
+            this.Controls.Add(this.SendMail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendMail;
+    }
+}
+
---
+++ GoogleEmail/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace GoogleEmail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMail_Click(object sender, EventArgs e)
+        {
+
+            string YuorGoogleAddress = "";
+            string YuorGooglePassword="";
+            System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+            //收信人
+            msg.To.Add("收件人Email");
+            msg.From = new MailAddress(YuorGoogleAddress, "寄件名稱", System.Text.Encoding.UTF8);
+            msg.Subject = "測試標題";//郵件標題
+            msg.SubjectEncoding = System.Text.Encoding.UTF8;//郵件標題編碼
+            msg.Body = "郵件內文"; //郵件內容
+            msg.BodyEncoding = System.Text.Encoding.UTF8;//郵件內容編碼 
+            //msg.Attachments.Add(new Attachment(@"D:\test2.docx"));  //附件
+            msg.IsBodyHtml = true;//是否是HTML郵件 
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential(YuorGoogleAddress, YuorGooglePassword); //帳號跟密碼
+
+            client.Host = "smtp.gmail.com"; //設定smtp Server
+            client.Port = 25; //設定Port
+            client.EnableSsl =true; //gmail預設開啟驗證
+            client.Send(msg); //寄出信件
+            client.Dispose();
+            msg.Dispose();
+            MessageBox.Show(this, "郵件寄送成功！");
+
+        }
+    }
+}
